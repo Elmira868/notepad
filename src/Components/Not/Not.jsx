8e7accf67161a95d3cef4e1ba645021f7d969542
elmira amjadian <elmiraamjadian5@gamil.com>
2024-12-03 19:12:48 +0330

    Change Design

--- src/Components/Not/Not.jsx
+++ src/Components/Not/Not.jsx
@@ -3,13 +3,12 @@ import './Not.css'
 import { BsEraser } from "react-icons/bs";
 export default class Not extends Component{
     render(){
+        let{id , title , color} = this.props
         return(
             <>
-            <div className="note">
-                <div className="note__container">
-                <span className="not-text">note1</span>
-                <button className="note__remove-btn"><BsEraser className="note__remove-icon" /></button>
-                </div>
+            <div className="note__container" style={{backgroundColor:color}}>   
+                <span className="not-text">{title}</span>
+                <button className="note__remove-btn" style={{backgroundColor:color}}><BsEraser className="note__remove-icon" /></button>
             </div>
             </>
         )
--- src/Components/Notepad/Notepad.jsx
+++ src/Components/Notepad/Notepad.jsx
@@ -20,12 +20,31 @@ export default class Notepad extends Component {
         this.noteTitleHandler = this.noteTitleHandler.bind(this);
         this.inputColorHandler = this.inputColorHandler.bind(this);
         this.emptyInput = this.emptyInput.bind(this);
+        this.addNote = this.addNote.bind(this);
+    }
+
+    // Add Notes
+    addNote(){
+let newNoteObg ={
+    id : this.state.notes.length + 1,
+    title: this.state.noteTitle,
+    color:this.state.inputColor
+}
+
+this.setState(prevState =>{
+    return{
+        notes:[...prevState.notes , newNoteObg],
+        inputColor:'#fff',
+        noteTitle:''
+    }
+})
     }
 
     // Empty Input Value
     emptyInput() {
         this.setState({
-            noteTitle: ""
+            noteTitle: "",
+            inputColor:'#fff'
         });
     }
 
@@ -47,24 +66,39 @@ export default class Notepad extends Component {
         return (
             <>
                 <h1 className="notePad__title">یاد داشت من</h1>
-                <input 
+                <div className="notepad__container">
+              <div className="notePad__box-items">
+              <input 
                     type="text" 
                     className="notePad__input" 
-                    placeholder="یادداشت" 
+                    placeholder="یادداشت"
                     style={{ backgroundColor: this.state.inputColor }} 
                     value={this.state.noteTitle} 
                     onChange={this.noteTitleHandler} 
                 />
                 <div className="notepad__btns-box">
-                    <button className="notepad-btn__add"><IoIosAdd className="notepad-btn__add-icon" /></button>
+                    <button className="notepad-btn__add" onClick={this.addNote}><IoIosAdd className="notepad-btn__add-icon" /></button>
                     <button className="notepad-btn__remove" onClick={this.emptyInput}>
                         <BsEraser className="notepad-btn__remove-icon" />
                     </button>
                 </div>
+
+                <div className="notePad__colors">
                 {this.state.colors.map(color => {
                     return <Colors color={color} key={color} onColor={this.inputColorHandler} />
                 })}
-                <Not />
+                </div>
+               
+              </div>
+
+                <div className="notePad__note">
+                {this.state.notes.map( note =>{
+                    return <Not {...note} />
+                })}
+                </div>
+
+                </div>
+                
             </>
         );
     }
